e74ea8291bdd572d6ad904379d414c6ea032e345
Amine Sadik <idekwiam18@gmail.com>
2025-09-04 10:25:45 +0100

    speed_transformation & rust script update

--- rust_update.zsh
+++ rust_update.zsh
@@ -1,1 +1,12 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# For your active (default) toolchain:
+rustup component add rust-src
+
+# If you also use nightly:
+rustup component add rust-src --toolchain nightly
+rustup component add rust-analyzer
+
+echo 'source "$HOME/.cargo/env"' >> ~/.zshrc                                                          ─╯
+exec $SHELL -l
+
+zsh
