180760f0ce32a31d2ec3f304d8f683a1f1132f34
Amine Sadik <idekwiam18@gmail.com>
2025-09-04 09:55:27 +0100

    scalar-1

---
+++ scalar/target/debug/libscalar.d
@@ -0,0 +1,1 @@
+/home/asadik/Desktop/piscine-rust/scalar/target/debug/libscalar.rlib: /home/asadik/Desktop/piscine-rust/scalar/src/lib.rs
---
+++ scalar/target/debug/scalar.d
@@ -0,0 +1,1 @@
+/home/asadik/Desktop/piscine-rust/scalar/target/debug/scalar: /home/asadik/Desktop/piscine-rust/scalar/src/lib.rs /home/asadik/Desktop/piscine-rust/scalar/src/main.rs
