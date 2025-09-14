332c145104b841cef1153ff50edba1421ac11bee
Amine Sadik <idekwiam18@gmail.com>
2025-09-14 21:46:40 +0100

    scores

--- scores/src/lib.rs
+++ scores/src/lib.rs
@@ -5,20 +5,16 @@ pub fn score(s: &str) -> String {
             'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => {
                 count += 1;
             }
-            'D' | 'G' => {
-                count += 2;
+            'F' | 'H' | 'V' | 'W' | 'Y' => {
+                count += 4;
             }
             'B' | 'C' | 'M' | 'P' => {
                 count += 3;
             }
-            'F' | 'H' | 'V' | 'W' | 'Y' => {
-                count += 4;
-            }
-            'K' => {
-                count += 5;
-            }
+            'D' | 'G' => count += 2,
             'J' | 'X' => count += 8,
             'Q' | 'Z' => count += 10,
+            'K' => count += 5,
             _ => count += 0,
         }
     }
