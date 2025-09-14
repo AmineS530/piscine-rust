55dbb4d581d3690301bbc44fc91f4ef781468c0a
Amine Sadik <idekwiam18@gmail.com>
2025-09-14 22:12:42 +0100

    pangram

---
+++ pangram/src/lib.rs
@@ -0,0 +1,13 @@
+use std::collections::HashSet;
+
+pub fn is_pangram(s: &str) -> bool {
+    let mut letters = HashSet::new();
+
+    for c in s.to_lowercase().chars() {
+        if c.is_ascii_alphabetic() {
+            letters.insert(c);
+        }
+    }
+
+    letters.len() == 26
+}
