221d5ba31d4349f99513b0bfdaab3b894bdfaae5
Amine Sadik <idekwiam18@gmail.com>
2025-09-14 21:59:58 +0100

    math isn't my strench point,failed attempt hh

--- searching/src/lib.rs
+++ searching/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn search(array: &[i32], key: i32) -> Option<usize> {
     for (idx, num) in array.iter().rev().enumerate() {
         if *num == key {
-            return Some(array.len() - 1 + idx);
+            return Some(array.len() - 1 - idx);
         }
     }
     None
