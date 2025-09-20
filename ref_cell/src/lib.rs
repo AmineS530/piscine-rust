b1a9b5f255c5d9f29d7b95b2c7bf9255cedddf00
Amine Sadik <idekwiam18@gmail.com>
2025-09-20 21:07:19 +0100

    ref_cell, whats the purpose of messenger.rs hh

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -1,28 +1,42 @@
-use std::rc::Rc;
 use std::cell::RefCell;
-
+use std::rc::Rc;
 
 pub struct Tracker {
     pub messages: Rc<RefCell<Vec<String>>>,
     pub value: RefCell<usize>,
-    pub max:usize,
+    pub max: usize,
 }
 
 impl Tracker {
-    pub fn new(max:usize) -> Self {
-        Tracker{
+    pub fn new(max: usize) -> Self {
+        Tracker {
             messages: Rc::new(RefCell::new(Vec::new())),
-            value:RefCell::new(0),
+            value: RefCell::new(0),
             max,
         }
     }
-    pub fn set_value(&self, new_val:&Rc<usize>) {
-        // let count = Rc::strong_count(new_val);
-        // if count > self.max {
-            // xxxx & peek
-        // }
-
+    pub fn set_value<T>(&self, new_val: &Rc<T>) {
+        let count = Rc::strong_count(new_val);
+        if count > self.max {
+            self.messages
+                .borrow_mut()
+                .push("Error: You can't go over your quota!".to_string());
+            return;
+        }
+        *self.value.borrow_mut() = count;
+        let per = count * 100 / self.max;
+        if per > 70 {
+            self.messages.borrow_mut().push(format!(
+                "Warning: You have used up over {}% of your quota!",
+                per
+            ));
+        }
     }
-    // pub fn peek(&self) {
-    // }
+    pub fn peek<T>(&self, new_val: &Rc<T>) {
+        let count = Rc::strong_count(new_val);
+        let per = count * 100 / self.max;
+        self.messages
+            .borrow_mut()
+            .push(format!("Info: This value would use {}% of your quota", per));
+    }
 }
