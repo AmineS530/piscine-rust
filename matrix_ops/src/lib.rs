7f2aa1db93f0bb53ea2ddacb0ca0fb97f1b66d9e
Amine Sadik <idekwiam18@gmail.com>
2025-09-28 00:30:04 +0100

    matrix_ops

--- matrix_ops/src/lib.rs
+++ matrix_ops/src/lib.rs
@@ -1,18 +1,50 @@
-mod matrix;
-mod scalar;
+use std::ops::{Add, Sub};
 
-use matrix::*;
+#[derive(Debug, PartialEq, Eq)]
 
-impl<T>std::ops::Add for Matrix<T> {
-    type Output = T;
-    fn add(self, other:Self) -> Self::Output {
-       
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl<T: Add<Output = T> + Copy> Add for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+    fn add(self, other: Self) -> Self::Output {
+        if self.0.len() != other.0.len() || self.0.is_empty() {
+            return None;
+        }
+        let mut res = Vec::new();
+
+        for (v1, v2) in self.0.iter().zip(other.0.iter()) {
+            if v1.len() != v2.len() {
+                return None;
+            }
+            let mut temp = Vec::new();
+            for (vv1, vv2) in v1.iter().zip(v2.iter()) {
+                temp.push(*vv1 + *vv2);
+            }
+            res.push(temp);
+        }
+        Some(Matrix(res))
     }
 }
 
-impl<T> std::ops::Sub for Matrix<T> {
-    type Output = T ;
+impl<T: Sub<Output = T> + Copy> Sub for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+
     fn sub(self, rhs: Self) -> Self::Output {
-        
+        if self.0.len() != rhs.0.len() || self.0.is_empty() {
+            return None;
+        }
+        let mut res = Vec::new();
+
+        for (v1, v2) in self.0.iter().zip(rhs.0.iter()) {
+            if v1.len() != v2.len() {
+                return None;
+            }
+            let mut temp = Vec::new();
+            for (vv1, vv2) in v1.iter().zip(v2.iter()) {
+                temp.push(*vv1 - *vv2);
+            }
+            res.push(temp);
+        }
+        Some(Matrix(res))
     }
 }
