8fea5eddee1f9a2fffb1cb1e967089f14ce4c35d
Amine Sadik <idekwiam18@gmail.com>
2025-09-29 17:58:44 +0100

    single product case

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,7 +1,9 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    if arr.len() == 1 {
+        return vec![];
+    }
     let n = arr.len();
     let mut result = vec![1; n];
-
     let mut prod = 1;
     for i in 0..n {
         result[i] = prod;
