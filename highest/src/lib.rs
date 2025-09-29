387be6e1c6cca77055c11bbf88f997adad35d495
Amine Sadik <idekwiam18@gmail.com>
2025-09-29 18:05:47 +0100

    crunchy keyboard

---
+++ highest/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct Numbers<'a> {
+    numbers: &'a [u32],
+}
+
+impl<'a> Numbers<'a> {
+    pub fn new(numbers: &'a [u32]) -> Self {
+        Numbers { numbers: numbers }
+    }
+
+    pub fn list(&self) -> &[u32] {
+        self.numbers
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.numbers.last().copied()
+    }
+
+    pub fn highest(&self) -> Option<u32> {
+        self.numbers.iter().max().copied()
+    }
+
+    pub fn highest_three(&self) -> Vec<u32> {
+        let mut nums: Vec<u32> = self.numbers.to_vec();
+        nums.sort();
+        nums.into_iter().rev().take(3).collect()
+    }
+}
